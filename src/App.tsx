a3c28a7ae678536e116b97904794743bad554033
Nikolai Lopin <code@lopin.me>
2019-08-25 21:52:27 +0200

    feat: use better tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import * as React from "react"
+import {useEffect, useState} from "react"
 import {Map, TileLayer} from "react-leaflet"
 
-
 export const App: React.FC = () => {
+    const [height, setHeight] = useState(window.innerHeight)
+
+    useEffect(() => {
+        const handleWindowResize = (): void => setHeight(window.innerHeight)
+
+        window.addEventListener('resize', handleWindowResize)
+        return (): void => {
+            window.removeEventListener('resize', handleWindowResize)
+        }
+    }, [])
+
     return (
         <>
-            <h1>Barcelona Coffee Map</h1>
-            <Map center={[41.3851, 2.1734]} zoom={13} style={{height: "500px"}}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <Map center={[41.3851, 2.1734]} zoom={13} style={{height: `${height}px`}}>
+                <TileLayer
+                    url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png"
+                    attribution="<a href='https://wikimediafoundation.org/wiki/Maps_Terms_of_Use'>Wikimedia</a>"
+                    minZoom={1}
+                    maxZoom={19}
+                />
             </Map>
         </>
     )
