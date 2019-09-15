19d7936f3c45ae8c90541ba08baca9a3ae5be3b7
Nikolai Lopin <code@lopin.me>
2019-09-15 19:10:34 +0200

    feat: add second coffee point

--- src/coffeepoints.ts
+++ src/coffeepoints.ts
@@ -7,7 +7,12 @@ export interface CoffeePoint {
 export const coffeePoints: ReadonlyArray<CoffeePoint> = [
     {
         "name": "Morrow Coffee",
-        "address": "Av. Gran Vía de les Corts Catalanes 403",
+        "address": "Av. Gran Vía de les Corts Catalanes, 403",
         "coordinates": [41.377229, 2.151273],
+    },
+    {
+        "name": "Espai Joliu",
+        "address": "Carrer de Badajoz, 95",
+        "coordinates": [41.398762, 2.1928493],
     }
 ]
